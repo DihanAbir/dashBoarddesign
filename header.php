29f05bb019c4b09b16b0ceb53ace69dd999b19b6
Dihan Abir <nahid.muradabir@gmail.com>
2020-03-08 20:34:52 +0600

    dash master first part

---
+++ header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/c1cce69857.js">
+    <script src="https://use.fontawesome.com/c1cce69857.js"></script>
+    <script src="https://kit.fontawesome.com/51e2507206.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <title>DashBoard Design</title>
+  </head>
+  <body>
---
+++ menubar.php
@@ -0,0 +1,71 @@
+<?php include "header.php" ?>
+
+<!-- -->
+<div class=" menu ">
+    <nav class="navbar navbar-expand-lg navbar-light ">
+      <a class="navbar-brand logo" href="#">Logo</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto navv ">
+      <li class="nav-item  ">
+        <a class="nav-link" href="#">Demos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pages</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#">Blog </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Portfolio</a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="#">Elements</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Support</a>
+      </li>
+       </ul>
+       
+
+       <ul class="navbar-nav ml-auto record1 ">
+        <li>
+          <button class="btn btn-outline-success record" type="submit">+Record</button>
+        </li>
+        <li class="nav-item dropdown navbar-nav ml-auto ">
+          <a class="nav-link dropdown-toggle email" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-user-alt "></i> nahid.muradabir@gmail.com
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+      </ul>
+   
+    </div>
+  </nav>
+</div>
+<!-- -->
+<!-- section devider -->
+<section>
+ <div class="underline"></div> 
+</section>
+
+<section class="coin">
+  <div class="container-fluid">
+   <div class="row">
+     <div class="col-md">
+        <div class="point my-4">
+          <img src="image/coin.png" alt="point">
+          <i class="fas fa-pen  "></i>
+        </div>
+      </div>
+   </div>
+  </div>   
+</section>
